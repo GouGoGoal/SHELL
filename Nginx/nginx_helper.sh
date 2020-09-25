af84a1380d41ba16c67165d1d435d3fffb99dc8b
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-09-25 12:09:22 +0800

    优化为并发执行，加快判断速度

--- Nginx/nginx_helper.sh
+++ Nginx/nginx_helper.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #旨在解决nginx转发动态域名时不会自动更新，以及缓解域名过多时出现的解析失败的现象
 #使用此脚本旧不要在nginx里使用resolver参数了，并按下方命令添加计划任务
-#echo "* * * * * root flock -xn /tmp/hosts.lock -c 'bash /etc/nginx/nginx_helper.sh'">>/etc/crontab
+#echo "* * * * * root flock -xn /dev/shm/hosts.lock -c 'bash /etc/nginx/nginx_helper.sh'">>/etc/crontab
 #不设置则使用系统DNS
 #DNS=223.5.5.5
 
-domain=( 
-	#"填写nginx需要转发的域名"
+#工作目录放至共享内存
+workdir="/dev/shm"
+
+domain=(
+	#填写域名，注意分号
 	"www.baidu.com"
 )
 
@@ -14,6 +17,7 @@ domain=(
 if [ ! -f "/etc/hosts.bak" ];then 
 	cp /etc/hosts /etc/hosts.bak
 fi
+
 #检测是否有host指令，如果没有就装一下
 if [ "`command -v host`" == "" ]; then
     echo "host命令安装中。。。"
@@ -23,33 +27,55 @@ if [ "`command -v host`" == "" ]; then
 	yum install -y bind-utils
     fi
 fi
-#定义规则是否改变
-is_changed=0
-#循环检测
-i=0
-rule=${domain[${i}]}
-while [ -n "$rule" ] 
-do
-	if [ "$DNS" == "" ];then
+
+#删除可能遗留的缓存
+rm -rf "$workdir"/hosts.change
+rm -rf "$workdir"/hosts
+rm -rf "$workdir"/hosts.tmp
+
+#将现有hosts拷贝至共享内存
+`which cp|tail -1` -f /etc/hosts "$workdir"/hosts
+
+if [ "$DNS" == "" ];then
+	for rule in ${domain[@]}
+	do
+		{
 		ip=`host $rule|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
-	else
-		ip=`host $rule $DNS|grep -v $DNS|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
-	fi
-	if [ "$ip" != "" ];then 
-		echo "$ip $rule">>/tmp/hosts_temp
-		if [ "`grep $ip /etc/hosts`" == "" ];then is_changed=1;fi
-	else continue
-	fi
-	let i++
-	rule=${domain[${i}]}
-done
-
-
-if [ "$is_changed" == "1" ];then 
-	`which cp|tail -1` -f /etc/hosts.bak /etc/hosts
-	cat /tmp/hosts_temp >>/etc/hosts
-	rm -rf /tmp/hosts_temp
-	systemctl reload nginx
+		if [ "$ip" != "" ];then 
+			echo "$ip $rule">>"$workdir"/hosts.tmp
+			if [ "`grep $ip "$workdir"/hosts`" == "" ];then 
+				touch "$workdir"/hosts.changed
+			fi
+		fi
+		}&
+	done
 else 
-	rm -rf /tmp/hosts_temp
+
+	for rule in ${domain[@]}
+	do
+		{
+		ip=`host $rule $DNS|grep -v $DNS|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
+		if [ "$ip" != "" ];then 
+			echo "$ip $rule">>"$workdir"/hosts.tmp
+			if [ "`grep $ip "$workdir"/hosts`" == "" ];then 
+				echo "1" >"$workdir"/hosts.changed
+			fi
+		else continue
+		fi
+		}&
+	done
 fi
+#等待全部判断完成
+wait
+
+if [ -f ""$workdir"/hosts.changed" ];then 
+	`which cp|tail -1` -f /etc/hosts.bak /etc/hosts
+	cat "$workdir"/hosts.tmp >>/etc/hosts
+	#systemctl reload nginx
+	echo "改变" 
+fi
+
+#删除缓存
+rm -rf "$workdir"/hosts.changed
+rm -rf "$workdir"/hosts
+rm -rf "$workdir"/hosts.tmp
