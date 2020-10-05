cb21172f40824a3a6a324ce8233f80c8ae4cf0be
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-10-05 12:04:38 +0800

    Update CloudFlare_BlackIP.sh

--- Nginx/CloudFlare_BlackIP.sh
+++ Nginx/CloudFlare_BlackIP.sh
@@ -1,46 +1,78 @@
 #/bin/bash
 #CloudFlare邮箱
-CFEMAIL="***@gmail.com"
+CFEMAIL="test@gmail.com"
 # 填CloudFlare API key
-CFAPIKEY="**********"
+CFAPIKEY="1111"
 #域名对应的ZonesID
-ZONESID="**********"
-
+ZONEID="1111"
 #监听哪个日志用以分析黑名单，根据实际修改
-logfile=/var/log/nginx/access.log
-#将IP提取到此文件，如果有多个网站请将此处修改成唯一值
-echofile=/tmp/black_list
-#黑名单IP列表，如果有多个网站请将此处修改成唯一值
-blackip=/tmp/black_ips
-#分析近几分钟的请求，计划任务频率根据此值适配，此处填1就每一分钟执行，填2就每两分钟执行
-last_minutes=1 
-#阈值，大概可以选择一分钟60次，同时根据上方的频率一起修改
-times=10
+logfile=/var/log/nginx/test.access.log
+#临时文件生成目录
+tmpfile=/dev/shm/$ZONEID
+#白名单IP，空格分割
+#whitelist=(1.1.1.1 2.2.2.2)
+#每隔多少秒分析一次，此处一般不需要更改
+cron=10s
+#阈值，大概可以选择10秒10-15次，同上方间隔一同修改
+times=15
+#模式 block challenge whitelist js_challenge
+mode=block
 
-
-
-start_time=`date +"%Y-%m-%dT%H:%M"`
-stop_time=`date -d "$last_minutes minutes ago" +"%Y-%m-%dT%H:"%M""`
-
-#过滤出单位之间内的日志并统计最高ip数，请替换为你的日志路径，日志切割格式可能需要根据场景修改，此处根据下方格式输出
-#    定义访问日志       #请求头传递源地址      #请求地址     #状态    #处理时长      #本地时间      #请求地址   #请求的url#请求浏览器信息
-#	log_format  access  $http_x_forwarded_for--$remote_addr--$status--$request_time--$time_iso8601--$http_host--$request--$http_user_agent;
-#本脚本根据“请求头传递源地址”进行分析
-sed -n "/$stop_time/,/$start_time/p" $logfile|awk -F '--' '{print $1}' | sort | uniq -c | sort -nr >$echofile
-ip_top=`cat $echofile | head -1 | awk '{print $1}'`
-ip=`cat $echofile | awk '{if($1>'$times')print $2}'`
-for line in $ip
+#删除可能遗留的临时文件
+rm -rf $tmpfile
+#解析白名单成IP
+rm -rf $tmpfile.whiteip
+for k in ${whitelist[*]}
 do
-echo $line >> $blackip
-#if [ "$line" != "1.1.1.1" ];then  #如果需要添加白名单，请取消此处注释
-curl -s -o /dev/null -X POST \
-    "https://api.cloudflare.com/client/v4/zones/$ZONESID/firewall/access_rules/rules" \
-    -H "X-Auth-Email: $CFEMAIL" \
-    -H "X-Auth-Key: $CFAPIKEY" \
-    -H "Content-Type: application/json" \
-    --data '{"mode":"block","configuration":{"target":"ip","value":"'$line'"},"notes":"脚本自动上报"}'   
-echo "$line $last_minutes分钟内访问站点大于$times次，已上报拉黑"
-#fi
+	{
+	if [ ! "`echo $k|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`" ];then
+		echo  "`host $k|grep -E -o \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"|head -1`">>$tmpfile.whiteip
+	else echo "$k">>$tmpfile.whiteip
+	fi
+	}&
 done
-rm -rf $echofile
-rm -rf $blackip
+wait
+#开始循环监听
+for ((x=0;x<60;x++)) 
+do
+	#监听日志
+	tail -f $logfile >$tmpfile &
+	#等待指定秒数
+	sleep $cron
+	#停止监听
+	kill $!
+	#过滤出单位之间内的日志并统计最高ip数，请替换为你的日志路径，日志切割格式可能需要根据场景修改，此处根据下方格式输出
+	#    定义访问日志       #请求头传递源地址      #请求地址     #状态    #处理时长      #本地时间      #请求地址   #请求的url#请求浏览器信息
+	#	log_format  access  $http_x_forwarded_for--$remote_addr--$status--$request_time--$time_iso8601--$http_host--$request--$http_user_agent;
+
+	#根据“请求头传递源地址”进行分析
+	accessip=`awk -F '--' '{print $1}' $tmpfile|sort|uniq -cd`
+	#如果单IP最大访问次数小于等于$times就直接退出脚本
+	if [ "`echo $accessip|awk '{print $1}'`" -lt "$times" ];then continue;fi
+	#如果IP访问次数大于等于$times就保存变量
+	ip=`echo "$accessip"|awk '{if($1>'$times')print $2}'`
+
+	#删除白名单中的IP
+	for i in `cat $tmpfile.whiteip`;do ip=`echo "$ip"|sed "/$i/d"`;done
+
+	#将上方记录的IP都统统拉黑
+	for j in $ip
+	do
+	{
+		curl -s -X POST \
+   			"https://api.cloudflare.com/client/v4/zones/$ZONEID/firewall/access_rules/rules" \
+    			-H "X-Auth-Email: $CFEMAIL" \
+    			-H "X-Auth-Key: $CFAPIKEY" \
+    			-H "Content-Type: application/json" \
+    			--data "{\"mode\":\"$mode\",\"configuration\"":{\""target\":\"ip\",\"value\":\"$j\"},\"notes\":\"脚本自动上报\"}"
+		echo "$line $last_minutes分钟内访问站点大于等于$times次，已上报拉黑"
+	}&
+	done
+	#等待上方循环结束
+	wait
+	#删除缓存文件
+	rm -rf $tmpfile
+done
+#执行完毕后重新运行脚本
+source "$0"
+
