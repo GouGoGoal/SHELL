f74e11ace258441b5a6c11fa2d4ced642ca2a4ee
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-01 14:21:07 +0800

    Update nginx_helper.sh

--- Nginx/nginx_helper.sh
+++ Nginx/nginx_helper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #旨在解决nginx转发动态域名时不会自动更新，以及缓解域名过多时出现的解析失败的现象
-#使用此脚本旧不要在nginx里使用resolver参数了，并按下方命令添加计划任务
+#若使用此脚本不要在nginx里使用resolver参数，并按下方命令添加计划任务
 #echo "* * * * * root flock -xn /dev/shm/hosts.lock -c 'bash /etc/nginx/nginx_helper.sh'">>/etc/crontab
-#不设置则使用系统DNS
-#DNS=223.5.5.5
 
-#工作目录放至共享内存
-workdir="/dev/shm"
+#缓存文件
+WorkFile="/dev/shm/nginx_helper"
 #通过命令提取nginx配置文件中的所有域名再排序去重，不要一个域名解析多个A记录，很频繁reload
 domain=(`grep -w server /etc/nginx/nginx.conf|grep -v '{'|awk '{print $2}'|awk -F ':' '{print $1}'|sort|uniq|grep -v -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`)
 
@@ -17,7 +15,6 @@ fi
 
 #检测是否有host指令，如果没有就装一下
 if [ "`command -v host`" == "" ]; then
-    echo "host命令安装中。。。"
     if [ ! -f "/etc/redhat-release" ]; then
         apt install -y dnsutils
     else 
@@ -26,52 +23,35 @@ if [ "`command -v host`" == "" ]; then
 fi
 
 #删除可能遗留的缓存
-rm -rf "$workdir"/hosts.change
-rm -rf "$workdir"/hosts
-rm -rf "$workdir"/hosts.tmp
+rm -rf $WorkFile.changed
+rm -rf $WorkFile.hosts
 
-#将现有hosts拷贝至共享内存
-`which cp|tail -1` -f /etc/hosts "$workdir"/hosts
-
-if [ "$DNS" == "" ];then
-	for rule in ${domain[@]}
-	do
-		{
-		ip=`host $rule|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
-		if [ "$ip" != "" ];then 
-			echo "$ip $rule">>"$workdir"/hosts.tmp
-			if [ "`grep $ip "$workdir"/hosts`" == "" ];then 
-				touch "$workdir"/hosts.changed
-			fi
-		fi
-		}&
-	done
-else 
-
-	for rule in ${domain[@]}
-	do
-		{
-		ip=`host $rule $DNS|grep -v $DNS|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
-		if [ "$ip" != "" ];then 
-			echo "$ip $rule">>"$workdir"/hosts.tmp
-			if [ "`grep $ip "$workdir"/hosts`" == "" ];then 
-				echo "1" >"$workdir"/hosts.changed
-			fi
-		else continue
-		fi
-		}&
-	done
-fi
-#等待全部判断完成
+for rule in ${domain[@]}
+do
+	{
+	ip=`host $rule|grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}'|sed -n '1p'`
+	if [ "$ip" ];then 
+		echo "$ip $rule">>$WorkFile.hosts
+		if [ ! "`grep $ip /etc/hosts`" ];then touch $WorkFile.changed;fi
+	else 
+		#若某域名未解析到IP，就设置为127.0.0.1 避免nginx启动失败
+		echo "127.0.0.1 $rule">>$WorkFile.hosts
+	fi
+	}&
+done
 wait
 
-if [ -f ""$workdir"/hosts.changed" ];then 
+if [ -f "$WorkFile.changed" ];then 
 	`which cp|tail -1` -f /etc/hosts.bak /etc/hosts
-	cat "$workdir"/hosts.tmp >>/etc/hosts
+	cat $WorkFile.hosts >>/etc/hosts
+	rm -rf $WorkFile.changed
 	systemctl reload nginx
 fi
+rm -rf $WorkFile.hosts
 
-#删除缓存
-rm -rf "$workdir"/hosts.changed
-rm -rf "$workdir"/hosts
-rm -rf "$workdir"/hosts.tmp
+#若nginx当前服务未启动就尝试手动重启
+if [ ! "`systemctl|grep nginx|grep running`" ];then 
+	systemctl restart nginx
+fi
+
+#* * * * * root if [ "`systemctl |grep nginx|grep running`" == "" ];then systemctl restart nginx;fi 一句话的命令
