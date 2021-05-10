84d150120290bad3380a9ca85c3c5864d0714531
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2021-05-10 11:21:46 +0800

    Update aliyun.sh

--- AliyunCLI/aliyun.sh
+++ AliyunCLI/aliyun.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-domain=test.com
+DomainName="10242018.xyz"
 
-#检测某个IP对应的端口是否开放，即检测此IP是否连通，正常返回1，不正常返回0，使用此函数请先安装nmap
+#功能：检测某个IP对应的端口是否开放，即检测此IP是否连通
+#      若开放1，若不开放输出0
+#用法：available_test IP或域名 端口
 function available_test()  {
 i=0
 while [ $i -lt 5 ];do
-	if [ "`nmap $1 -p $2|grep open`" == "" ];then	
+	if [ "`nmap $1 -p $2|grep open`" ];then	
+		echo 1
+	else 
 		let i++
 		sleep 5s
-	else 
-		return 1;
 	fi
 done
-return 0;
+echo 0
 }
+#功能：根据端口开闭来启停解析
+#用法：AutoChangeStatus 域名或IP 端口 对应的RecordID
+function AutoChangeStatus() {
+	x=`available_test $1 $2`
+	case $x in
+	0)
+		if [ ! -f "/dev/shm/$1" -o "`cat /dev/shm/$1`" == 1 ];then
+			aliyun alidns SetDomainRecordStatus --RecordId $3 --Status Disable
+			echo '0' >/dev/shm/$1
+		fi
+		;;
+	1)
+		if [ ! -f "/dev/shm/$1" -o "`cat /dev/shm/$1`" == 0 ];then
+			aliyun alidns SetDomainRecordStatus --RecordId $3 --Status Enable
+			echo '1' >/dev/shm/$1
+		fi
+		;;
+	esac
+}
+
 #	if [ "`command -v nmap`" == "" ];then
 #		if [ ! -f "/etc/redhat-release" ];then
 #			apt install -y nmap
@@ -26,39 +48,31 @@ return 0;
 function help() {
 echo '
 	aliyun.sh config 初始化配置
-			list [RR] 列出当前所有解析(最多显示500个解析)，可加筛选字段
-			enable/disable/del 00000000000000000  启用/停用/删除此RecordID解析
-			add A|CNAME 1.1.1.1|test.com 添加一个解析
-			change 00000000000000000 1.1.1.1|test.com [A|CNAME] 更改此RecordID解析(解析类型默认不变，若想更改用最后一个参数指定)
-			cron 执行预设定时任务'
+		list [RR] 列出当前所有解析(最多显示500个解析)，可加筛选字段
+		enable/disablei/del 00000000000000000  启用/停用/删除此RecordID解析
+		add A|CNAME 1.1.1.1|test.com 添加一个解析
+		change 00000000000000000 1.1.1.1|test.com [A|CNAME] 更改此RecordID解析(解析类型默认不变，若想更改用最后一个参数指定)
+		cron 执行预设定时任务'
 }
 
-
+#定时任务 
 function cron() {
-	#检测22端口是否连通，若没有就自动停掉对应的解析
-	available_test test.test.com 22  
-	if [ "$?" == "0" ];then 
-		aliyun alidns SetDomainRecordStatus --RecordId 00000000000000000 --Status Disable
-	fi
+	#AutoChangeStatus www.baidu.com 80 1111111111111111 &
+	wait
+	stty echo
 }
 
 
-
-
-
-
-
-#case不区分大小写
+#不匹配大小
 shopt -s nocasematch
 case $1 in
 list)
 	if [ "$2" == "" ];then 
-				tables="`aliyun alidns  DescribeDomainRecords --DomainName $domain --output cols=RR,Value,TTL,RecordId,Status rows=DomainRecords.Record[] --PageSize 500`"
-				echo "$tables"|head -2
-				echo "$tables"|sed '1,2d'|sort
-
+		tables="`aliyun alidns  DescribeDomainRecords --DomainName $DomainName --output cols=RR,Value,TTL,RecordId,Status rows=DomainRecords.Record[] --PageSize 500`"
+		echo "$tables"|head -2
+		echo "$tables"|sed '1,2d'|sort
 	else 
-		aliyun alidns  DescribeDomainRecords --DomainName $domain --output cols=RR,Value,TTL,RecordId,Status rows=DomainRecords.Record[] --PageSize 500 |grep $2|sort
+		aliyun alidns  DescribeDomainRecords --DomainName $DomainName --output cols=RR,Value,TTL,RecordId,Status rows=DomainRecords.Record[] --PageSize 500 |grep $2|sort
 	fi
 	;;
 enable)
@@ -99,16 +113,3 @@ cron)
 	help
 	;;
 esac
-
-
-
-
-
-
-
-
-
-
-
-
-
