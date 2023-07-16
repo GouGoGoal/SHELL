710eac0eee751c83fb5ddcb766ef1f72bdf2a622
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2023-07-16 16:49:47 +0800

    update

--- sysctl.sh
+++ sysctl.sh
@@ -34,8 +34,8 @@ net.ipv4.tcp_tw_reuse=1
 net.ipv4.tcp_fin_timeout=30
 #表示当keepalive起用的时候，TCP发送keepalive消息的频度。缺省是2小时，改为2分钟。
 net.ipv4.tcp_keepalive_time=120
-#用于向外连接的端口范围。缺省情况下很小：32768到61000，改为1024到65000。
-net.ipv4.ip_local_port_range=1024 65000
+#用于向外连接的端口范围。缺省情况下很小：32768到61000，改为10000到65000，建议业务端口取10000以下。
+net.ipv4.ip_local_port_range=10000 65000
 #表示SYN队列的长度，默认为1024，加大队列长度为8192，可以容纳更多等待连接的网络连接数。
 net.ipv4.tcp_max_syn_backlog=8192
 #表示系统同时保持TIME_WAIT套接字的最大数量
